d780ca84bcae73c65b7a9f05f17f640c7d0201b1
SongChangPeng <songcpios@163.com>
2018-11-28 18:45:09 +0800

    add file

---
+++ ClothingSecurity/ClothingSecurity/ScanResultViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ScanResultViewController.swift
+//  ClothingSecurity
+//
+//  Created by 宋昌鹏 on 2018/11/28.
+//  Copyright © 2018 scpUpCloud. All rights reserved.
+//
+
+import Foundation
+
+class ScanResultViewController: BaseViewController {
+    
+}
